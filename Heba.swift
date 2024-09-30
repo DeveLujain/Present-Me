bda2a577db94e7e1b6619960478c1c84cadf0912
HebaDes <hebah2234@gmail.com>
2024-09-30 09:18:40 +0300

    HHH

---
+++ Heba.swift
@@ -0,0 +1,18 @@
+//
+//  Heba.swift
+//  FIRSTATTEMPT
+//
+//  Created by Heba Almohaisn on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Heba: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Heba()
+}
